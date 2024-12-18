d764a9448fa757fcd6ad21c8522cf26636e499c6
erfan taheri <erfanta197@gmail.com>
2024-12-18 15:24:03 +0330

    a simple guess word using js

---
+++ index.js
@@ -0,0 +1,73 @@
+const file = require("./file.json");
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const askQuestion = (question) =>
+  new Promise((resolve) =>
+    readline.question(question, (input) => resolve(input))
+  );
+
+const generateRandom = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+const getRandomWord = (file) => file[generateRandom(0, file.length - 1)];
+
+const isValidGuess = (guess, answer, word) => {
+  const errors = [
+    {
+      condition: !/[a-z]/.test(guess),
+      message: new Error("please guess a-z"),
+    },
+    {
+      condition: guess.length > 1,
+      message: new Error("please write 1 charachter"),
+    },
+    {
+      condition: answer.includes(guess),
+      message: new Error("you alread guess this CH"),
+    },
+    {
+      condition: !word.includes(guess),
+      message: new Error("wrong guss"),
+    },
+  ];
+
+  for (let error of errors) if (error.condition) throw error.message;
+};
+
+const arangeAnswer = (guess, answer, word) => {
+  word.forEach((ch, i) => {
+    if (ch === guess) answer[i] = guess;
+  });
+
+  console.log(answer.join(""));
+};
+
+const isWiner = (answer) => !answer.includes("-");
+
+const main = async () => {
+  const word = getRandomWord(file).split("");
+  const answer = new Array(word.length).fill("-");
+  console.log(word.join(""));
+
+  while (true) {
+    const guess = await askQuestion("guess a letter: ");
+    try {
+      isValidGuess(guess, answer, word);
+
+      arangeAnswer(guess, answer, word);
+
+      if (isWiner(answer)) {
+        console.log("you won");
+        readline.close();
+        return;
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+};
+
+main();
